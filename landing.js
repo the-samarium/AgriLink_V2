27189130eab7ae9624638548ae59b93e4bfe1e30
the-samarium <tropicalvoilet008@gmail.com>
2025-03-28 20:41:37 +0530

    Landing complete

--- landing.js
+++ landing.js
@@ -1,2 +1,15 @@
 
 // To do light mode toggle
+
+function toggleAccordion(id) {
+    const content = document.getElementById(`content-${id}`);
+    const icon = document.getElementById(`icon-${id}`);
+    
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+        icon.innerHTML = '-';
+    } else {
+        content.classList.add('hidden');
+        icon.innerHTML = '+';
+    }
+}
